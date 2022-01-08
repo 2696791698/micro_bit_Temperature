9620c662092e0e1c898b43e5f3c2d6d87f2a1571
2696791698 <76392624+2696791698@users.noreply.github.com>
2022-01-08 12:50:58 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(最高温度)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(最低温度)
+})
+let 最低温度 = 0
+let 最高温度 = 0
+最高温度 = -100000000
+最低温度 = 100000000
+basic.forever(function () {
+    if (最高温度 < input.temperature()) {
+        最高温度 = input.temperature()
+    }
+    if (最低温度 > input.temperature()) {
+        最低温度 = input.temperature()
+    }
+})
